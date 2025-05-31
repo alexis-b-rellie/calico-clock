0f78ded5c86845f597de0172e9e8f24430ef7dcb
Alexis <alexis.stephens2@education.nsw.gov.au>
2025-05-31 17:50:26 +1000

    Routing
    Added routing and HTML templates for login/signup/dashboard

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
-    return "Hello from Calico Clock!"
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 
 
 if __name__ == '__main__':
