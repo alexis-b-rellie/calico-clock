55eaa6a05d8a944d6fbacff52b29b62b88eadf82
Alexis <alexis.stephens2@education.nsw.gov.au>
2025-05-31 18:18:42 +1000

    Fixed issue
    Fix for the no-show Home page

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    print("Rendering index.html")
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        # Here you would check username/password
+        return redirect(url_for('dashboard'))
     return render_template('login.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        # Here you would create the user
+        return redirect(url_for('dashboard'))
     return render_template('signup.html')
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
+@app.route('/test')
+def test():
+    return "Hello, Flask is working!"
 
 if __name__ == '__main__':
     app.run(debug=True)
