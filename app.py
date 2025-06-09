4aa91a800ce0bd3c97c2609b733b8ff97eebef44
Alexis <alexis.stephens2@education.nsw.gov.au>
2025-06-09 20:16:30 +1000

    Log in and Sign up
    Added a finalized login and signup field. Using .db to store data.

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+# Create the database if it doesn't exist
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
@@ -11,15 +25,31 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # Here you would check username/password
-        return redirect(url_for('dashboard'))
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            return redirect(url_for('dashboard'))
+        else:
+            return "Invalid username or password!"
     return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        # Here you would create the user
-        return redirect(url_for('dashboard'))
+        username = request.form['username']
+        password = request.form['password']
+        # Check if user already exists
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return "Username already exists!"
+        # Create new user
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
     return render_template('signup.html')
 
 @app.route('/dashboard')
